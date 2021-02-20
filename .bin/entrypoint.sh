e2c9a66a8072d579902d487a4d4d37c18adfd007
Maxim Usikov <mu.usikov@gmail.com>
2021-02-20 16:15:23 +0300

    fix: set workdir for action container
    Move to /commitlint directory

--- .bin/entrypoint.sh
+++ .bin/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -eou pipefail
 
-PATH="node_modules/.bin:${PATH}"
+COMMITLINT_HOME="/commitlint"
+COMMITLINT_CONFIG="${COMMITLINT_HOME}/commitlint.config.js"
+PATH="${COMMITLINT_HOME}/node_modules/.bin:${PATH}"
 
-COMMITLINT_CONFIG="./commitlint.config.js"
 COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
 
 echo "${COMMIT_MESSAGE}" | commitlint --config "${COMMITLINT_CONFIG}" --verbose
